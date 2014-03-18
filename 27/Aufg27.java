130eb12fe568724019708e12fb9fff2b0ad18b0f
Oliver Steenbuck <git@hexren.net>
2014-03-18 12:39:55 +0100

    add cache aufg 27

--- 27/Aufg27.java
+++ 27/Aufg27.java
@@ -1,15 +1,20 @@
+import java.util.Map;
+import java.util.HashMap;
+
 public class Aufg27{
 
-	public static void main(String[] args){
+	public static Map<Long, Boolean> cache = new HashMap<Long, Boolean>(); 
 
+	public static void main(String[] args){
 		int a = 0;
 		int b = 0;
 		int n = 0;
 		int maxLen = 0;
 		int maxA = 0;
 		int maxB = 0;
-		for(a = -1000; a < 1000; a++){	
-			for(b = -1000; b < 1000; b++){
+		for(a = -1000; a < 1000; a = a+1){	
+	
+			for(b = -1000; b < 1000; b = b+1){	
 				int len = 0;
 				//System.out.println("a: " + a + " b: " + b);
 				for(n = 0; true; n++){
@@ -39,12 +44,23 @@ public class Aufg27{
 	}
 
 	public static boolean isPrime(long n){
-    	if(n <= 1) 
+		n = Math.abs(n);
+		if(cache.containsKey(n))
+			return cache.get(n);
+    	
+		if(n <= 1) 
 			return false;
-		for(int i = 2; i <= Math.sqrt(n)+1; i++){
-			if(n % i == 0)
+		if(n % 2== 0)
+			return false;
+		
+		for(int i = 3; i <= Math.sqrt(n)+1; i = i + 2){
+			if(n % i == 0){
+				cache.put(n,false);
 				return false;
+		
+			}
 		}
+		cache.put(n,true);
 		return true;
 	}
 }

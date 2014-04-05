f84c592edbe1dc24590148d19692f2676cc97b08
Oliver Steenbuck <git@hexren.net>
2014-04-05 18:01:12 +0200

    aufg 44

---
+++ 44/Aufg44.java
@@ -0,0 +1,18 @@
+public class Aufg44{
+
+    public static List<Integer> pentagonals = new ArrayList<Integer>();
+
+    public static void main(String[] args){
+         
+    }
+
+    public static int nextPenNu(int n){
+        
+        int pen = 0;
+        while(pen <= n){
+        
+        }
+        
+    }
+
+}

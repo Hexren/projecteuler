fd4fe6ae99c7803e1d32167a1edf3199b8e172e8
Oliver Steenbuck <git@hexren.net>
2014-04-11 22:08:17 +0200

    add 51 temp

--- 51/Aufg51.java
+++ 51/Aufg51.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.Set;
+import java.util.HashSet;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -6,26 +8,72 @@ import java.util.Iterator;
 import java.util.Collections;
 
 public class Aufg51{
-
+        public static Primes p;
 	public static void main(String[] args){
-		Primes p = new Primes(100);
+		p = new Primes(100000);
 		List<Integer> pl = p.asList();
-		
-
+		pl = getPrimesWithXDigits(pl, 5);
+        //System.out.println(pl.get(0));
+        System.out.println(getReplacementCands(toDigits(56003)));
+        for(Integer prime: pl){
+            for(Tuple replacement: getReplacementCands(toDigits(prime))){
+                //System.out.println("Testing:" + prime + " with replacement:" + replacement);       
+                List<Integer> primes = new ArrayList<Integer>(8);
+                for(int i = 0; i <= 9; i++){
+                    int newNumber = replaceDigits(replacement, prime, i);
+                    if(isPrime(newNumber)){
+                        primes.add(newNumber);
+                    }
+                }
+                if(primes.size()>=7){
+                    Collections.sort(primes);
+                    System.out.println(replacement);
+                    System.out.println(primes);
+                    System.out.println("Found! exiting...");
+                    System.exit(0);
+                }
+       
+            }
+        }
 	}
 
+    public static int replaceDigits(List<Integer> digits, int original, int replacement){
+        List<Integer> origDig = toDigits(original);
+        for(Integer i: digits){
+            origDig.set(i, replacement);
+        }
+        return listToInt(origDig);
+    }
+    
 	public static List<Integer> getPrimesWithXDigits(List<Integer> primes, int x){
-		
 		int i = 0;
 		for(i= 0; i<primes.size(); i++){
 			if(toDigits(primes.get(i)).size() >= x)
-				return primes.subList(x-1,primes.size());
+				return primes.subList(i,primes.size());
 		}
 		return primes;
 	}
 
+    public static boolean isPrime(int n){
+        return p.isPrime(n);
+    }
+
+    //find candidate replacements in a primes
+    public static Set<Tuple> getReplacementCands(List<Integer> digits){
+        Set<Tuple> candidates = new HashSet<Tuple>(); 
+        for(int i=0; i<digits.size()-1; i++){
+            for(int j=i+1; j <digits.size();j++)
+                if(digits.get(i) == digits.get(j)){
+                    candidates.add(new Tuple(i,j));
+                }
+        }
+        return candidates;
+        
+    } 
+
+    //compares 2 numbers given as digit lists
 	public static boolean equal(List<Integer> p1, List<Integer> p2, int ia, int ib){
-		for(int i=0; i<=p1.size(); i++){
+		for(int i=0; i<p1.size(); i++){
 			if(i == ia || i == ib)
 				continue;
 			if(p1.get(i) != p2.get(i)){
@@ -46,7 +94,13 @@ public class Aufg51{
 		return new ArrayList<Integer>(Arrays.asList(numbers));
 	}
 
-
+    public static int listToInt(List<Integer> digits){
+        String n = "";
+        for(Integer i: digits){
+            n = n + i;
+        }
+        return Integer.parseInt(n);
+    }
 
     public static class Primes implements Iterable<Integer>{
         
@@ -94,7 +148,36 @@ public class Aufg51{
             
 		    return primes;
 	    }
-
-
     }
+
+    public static class Tuple{ 
+        public final int x; 
+        public final int y; 
+        public Tuple(int x, int y) { 
+            this.x = x; 
+            this.y = y; 
+        } 
+
+        public int hashCode() {
+        	return (int)(y+x); 
+    	}
+
+		public boolean equals(Object other) {
+			//System.out.println(this.toString());
+			//System.out.println(other.toString());
+			if (other.getClass() != getClass()) 
+      			return false; 
+    		if (x != ((Tuple)other).x) 
+       			return false;
+			if (y != ((Tuple)other).y) 
+       			return false;
+			return true;
+   		} 
+
+        public String toString(){
+            return "{"+x+","+y+"}";
+        }
+
+    } 
+
 }

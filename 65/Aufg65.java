ade1a3d8085c6dfec35a8841f9abcc27014f1d57
Oliver Steenbuck <git@hexren.net>
2014-04-30 14:36:08 +0200

    finish aufg65

--- 65/Aufg65.java
+++ 65/Aufg65.java
@@ -1,32 +1,68 @@
 import java.math.BigInteger;
+import java.util.List;
+import java.util.Collections;
+import java.util.ArrayList;
 
 public class Aufg65{
 
-	public static void main(String[] args){
-		BigInteger a = BigInteger.valueOf(2);
-        BigInteger h1 = BigInteger.valueOf(1);
-        BigInteger h2 = BigInteger.valueOf(1);
-        BigInteger k1 = BigInteger.valueOf(1);
-        BigInteger k2 = BigInteger.valueOf(0);
+    public static int x = 1;
         
-        int counter = 0;
 
+	public static void main(String[] args){
+		BigInteger a;
+        BigInteger h1 = BigInteger.valueOf(1);
+        BigInteger h2 = BigInteger.valueOf(0);
+        BigInteger k1 = BigInteger.valueOf(0);
+        BigInteger k2 = BigInteger.valueOf(1);
+        BigInteger h = null;
+        BigInteger k = null;
+
+        int counter = 0;
         //see infinite continued fractions on wikipedia
-        for(int i=0; i<10 ;i++){
-            BigInteger h = a.multiply(h1).add(h2); 
-            BigInteger k = a.multiply(k1).add(k2); 
+        for(int i=1; i<=100 ;i++){
+            //System.out.println(contFrac(i));          
+            a = BigInteger.valueOf(contFrac(i));
+
+            h = a.multiply(h1).add(h2); 
+            k = a.multiply(k1).add(k2); 
             
             System.out.println(h + "/" + k);
-
             
             h2 = h1;
             h1 = h;
             k2 = k1;
             k1 = k;
+
         }
-        System.out.println("Result: " + counter);
+        
+        int res = 0;
+        for(Integer i: toDigits(h)){
+            res += i;
+        }
+
+        System.out.println("Result: " + res);
 
     }
 
+    //see http://oeis.org/A003417
+    public static int contFrac(int n){
+        if(n==1)
+            return 2;        
+        if(n%3==0)
+            return (n/3)*2;
+        else
+            return 1;
+    }
+
+    public static List<Integer> toDigits(BigInteger n){
+		List<Integer> digits = new ArrayList<Integer>();
+
+		while(n.compareTo(BigInteger.ZERO) > 0){
+			digits.add(n.mod(BigInteger.TEN).intValue());
+			n = n.divide(BigInteger.TEN);
+		}
+		Collections.reverse(digits);
+		return digits;
+	}
 
 }

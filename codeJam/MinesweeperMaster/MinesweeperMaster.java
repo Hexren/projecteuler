6c022e40b3b31e28c0358c6ae546c086488dd180
Oliver Steenbuck <oliver@steenbuck.org>
2014-10-14 15:16:18 +0200

    field evaluation start

--- codeJam/MinesweeperMaster/MinesweeperMaster.java
+++ codeJam/MinesweeperMaster/MinesweeperMaster.java
@@ -1,10 +1,18 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.math.BigInteger;
-
+import java.util.Arrays;
 
 public class MinesweeperMaster{
 	
+	public static String MINE = "x";
+	public static String CLICK = "C";
+	public static String OPEN = "O"; 
+	public static String NUMBER = "N";
+	public static String HIDDEN = "H";	 
+
+
+
 	public static void main(String[] args) throws Exception{
 		Problem[] problems = getProblems();
 		for(Problem problem: problems){
@@ -20,22 +28,102 @@ public class MinesweeperMaster{
     public static void solveField(Problem problem){
         String[][] field = new String[problem.r][problem.c];
         int m = 0;
+        for(int i=field.length-1; i>=0; i--){
+            for(int j=field[i].length-1; j>=0; j--){
+                if(m < problem.m){
+                    field[i][j]=MINE;
+                    m++;
+                }else{
+					field[i][j]=".";
+				}
+            }
+        }  
+		printField(field);  
+		field=markFields(field);
+		printField(field);
+ 
+    }
+
+	public static void printField(String[][] field){
         for(int i=0; i<field.length; i++){
             for(int j=0; j<field[i].length; j++){
-                if(m < problem.m){
-                    System.out.print("x");
-                    m++;
-                }else{  
-                    System.out.print(".");                  
-                }
+            	System.out.print(field[i][j]);
             }
             System.out.println("");
         }     
-    }
+	}
 
-    public static void solveField2(Problem problem){
 
-    }
+	//returns true if one of the neighbors is CLICKED or OPEN
+	public static boolean hasOpenNeig(int x, int y, String[][] field){
+		String[] ones = {CLICK, OPEN};
+		if(assertFieldHasOne(x,y,ones, field))
+			return true;
+		return false;
+	}
+
+	//return true if the field borders a mine 
+	public static boolean hasMineNeig(int x, int y, String[][] field){
+		String[] ones = {MINE};
+		if(assertFieldHasOne(x,y,ones, field))
+			return true;
+		
+		return false;
+	}
+
+	//return true if a neighbor of field[x][y] contains something from ones
+	public static boolean assertFieldHasOne(int x, int y, String[] ones, String[][] field){
+		for(int i=Math.max(x-1,0); i<=Math.min(field.length-1,x+1); i++){
+			for(int j=Math.max(y-1,0); j<=Math.min(field[i].length-1,y+1) ;j++){
+				if(Arrays.asList(ones).contains(field[i][j]))
+					return true;
+			}
+		}
+		return false;
+	}
+
+
+	public static boolean isLegal(String[][] field){
+		String[][] markedField= markFields(field);
+		boolean res = checkFields(markedField);
+		return res;
+	}
+
+	//marks left bottom as click and reveals according to rules
+	public static String[][] markFields(String[][] field){
+		String[][] markedField = cloneField(field);		
+		markedField[0][0] = CLICK;
+ 		for(int i=0; i<markedField.length; i++){
+            for(int j=0; j<markedField[i].length; j++){
+				
+				if(markedField[i][j]==CLICK){				
+	
+                }else if(hasOpenNeig(i,j,markedField) && markedField[i][j]!=MINE){
+					
+                    if(!hasMineNeig(i,j,markedField)){
+						markedField[i][j]=OPEN;
+					}else{
+						markedField[i][j]=NUMBER;
+					}
+                }else if(markedField[i][j]!=MINE){
+					markedField[i][j]=HIDDEN;
+				}
+            }
+        }  
+		return markedField;
+	}
+
+	public static boolean checkFields(String[][] field){
+		return true;
+	}
+
+
+	public static String[][] cloneField(String[][] field){
+		String [][] myField = new String[field.length][];
+		for(int i = 0; i < field.length; i++)
+    		myField[i] = field[i].clone();
+		return myField;
+	}
 
 	public static Problem[] getProblems() throws Exception{
         BufferedReader reader = new BufferedReader(new FileReader("C-small-practice.in"));
